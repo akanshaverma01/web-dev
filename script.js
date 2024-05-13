3ce0a4fea8bab439fdfd96b1b8c7984abefed0c6
akanshaverma01 <akanshasinghakuu@gmail.com>
2024-05-13 10:05:41 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,106 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+
+
+function firstPageAnim() {
+    var tl = gsap.timeline();
+
+
+    tl.from("#nav", {
+        y:'-10',
+        opacity: 0,
+        duration: 1.5,
+        ease: Expo.easeInOut
+    })
+    .to(".boundingelem", {
+        y:0,
+        duration: 2,
+        ease: Expo.easeInOut,
+        delay:-1,
+        stagger:.2
+        
+    })
+
+    
+    tl.from("#herofooter", {
+        y:-10,
+        opacity: 0,
+        duration: 1.5,
+        delay:-1,
+        ease: Expo.easeInOut
+    })
+
+}
+
+function circlechaptakaro(){
+    var xscale = 1;
+    var yscale = 1;
+
+    var xprev = 0;
+    var yprev = 0;
+
+    window.addEventListener("mousemove", function(dets) {
+        //  var xdiff = dets.clientX - xprev;
+        //  var ydiff = dets.clientY - yprev;
+         xscale = gsap.utils.clamp(.8,1.2, dets.clientX-xprev);
+         yscale = gsap.utils.clamp(.8,1.2, dets.clientY - yprev);
+
+         xprev = dets.clientX;
+         yprev = dets.clientY;
+
+         circleMouseFollower(xscale, yscale);
+
+    
+    });
+
+    
+}
+circlechaptakaro();
+
+function circleMouseFollower(xscale, yscale) {
+    window.addEventListener("mousemove", function (dets){
+        this.document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px,${dets.clientY}px) scale(${xscale}, ${yscale})`;
+
+    })
+}
+
+circleMouseFollower();
+firstPageAnim();
+
+document.querySelectorAll(".elem").forEach(function (elem) {
+    var rotate = 0;
+    var diffrot = 0;
+
+
+
+
+
+    elem.addEventListener("mouseleave", function (dets) {
+        gsap.to(elem.querySelector("img"), {
+            opacity: 0,
+            ease: Power3,
+         
+        });
+    });
+
+
+
+    elem.addEventListener("mousemove", function (dets) {
+        var diff = dets.clientY - elem.getBoundingClientRect().top;
+        diffrot = dets.clientX - rotate;
+        rotate = dets.clientX;
+
+
+        gsap.to(elem.querySelector("img"), {
+            opacity: 1,
+            ease: Power3,
+            top: diff,
+            left: dets.clientX,
+            rotate: gsap.utils.clamp(-20, 20, diffrot * 0.5),
+        });
+    });
+
+});
